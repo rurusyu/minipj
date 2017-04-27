a021a4943b9698058d61d47f5d27bcdf7da1a53c
JAVIS <JAVIS@DESKTOP-V7NGNCH>
2017-04-27 16:40:38 +0900

    수정

--- mini/src/main/java/org/gearss/controller/FrontController.java
+++ mini/src/main/java/org/gearss/controller/FrontController.java
@@ -3,9 +3,11 @@ package org.gearss.controller;
 import javax.inject.Inject;
 
 import org.apache.log4j.Logger;
+import org.gearss.domain.ImgVO;
 import org.gearss.service.ImgService;
 import org.gearss.service.ReplyService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,11 +30,19 @@ public class FrontController {
 	}
 	
 	@GetMapping("/write")
-	public void writeGET(){
-		logger.info("write..");
+	public void writeGET(ImgVO vo, Model model) throws Exception{
+		logger.info("쓰기 성공적");
 	}
 	
-	@PostMapping("/re")
+	@PostMapping("/write")
+	public String writePOST(ImgVO vo, Model model) throws Exception{
+		
+		service.regist(vo);
+		model.addAttribute("result", "성공적으로 등록되었습니다.");
+		
+		return "redirect:/picboard/main";
+		
+	}
 	
 	
 	@GetMapping("/read")
